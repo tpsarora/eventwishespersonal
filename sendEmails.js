6c4aff4b1712932645e9d5f3b72d644acc581118
Malika Arora <malika.arora@citrix.com>
2025-01-22 01:28:12 +0530

    date

--- sendEmails.js
+++ sendEmails.js
@@ -1,15 +1,15 @@
 ﻿const admin = require("firebase-admin");
 const emailjs = require("@emailjs/browser");
-require('dotenv').config(); // Load environment variables from .env file
-
+require("dotenv").config(); // Load environment variables from .env file
 
 // Load Firebase service account key from environment variable
 // console.log("Printing env ",process.env);
 // console.log("Printing env ",process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
-const serviceAccountKey = Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT_KEY, 'base64').toString('utf8');
+const serviceAccountKey = Buffer.from(
+  process.env.FIREBASE_SERVICE_ACCOUNT_KEY,
+  "base64"
+).toString("utf8");
 const serviceAccount = JSON.parse(serviceAccountKey);
-
-
 
 const FIREBASE_CONFIG = {
   apiKey: "AIzaSyCfbqC-893YBqhgR5OR0eHyX-EXzCIoTC8",
@@ -19,16 +19,14 @@ const FIREBASE_CONFIG = {
   storageBucket: "jwdatabase-f0e20.appspot.com",
   messagingSenderId: "1335908909",
   appId: "1:1335908909:web:0c132d7c79008c490c36a4",
-  measurementId: "G-BQG1N754TN"
+  measurementId: "G-BQG1N754TN",
 };
 
-const EMAILJS_SERVICE_ID = 'service_jitwsrj';
+const EMAILJS_SERVICE_ID = "service_jitwsrj";
 
-const EMAILJS_TEMPLATE_ID = 'template_m0u22pm';
+const EMAILJS_TEMPLATE_ID = "template_m0u22pm";
 
-const EMAILJS_USER_ID = 'qcbXaXrWGMaIRt6_o';
-
-
+const EMAILJS_USER_ID = "qcbXaXrWGMaIRt6_o";
 
 // Load Firebase config from environment variables
 //console.log(process.env);
@@ -43,9 +41,14 @@ const db = admin.firestore();
 
 async function sendEmails() {
   console.log("Sending emails...");
+
   const today = new Date();
-  const todayFormatted = today.toISOString().split("T")[0];
-  console.log("Today: ", todayFormatted);
+  const indiaTimeOffset = 5.5 * 60 * 60 * 1000; // IST is UTC+5:30
+  const indiaTime = new Date(today.getTime() + indiaTimeOffset);
+  const todayFormatted = indiaTime.toISOString().split("T")[0];
+
+  console.log("India Date:", todayFormatted);
+
   try {
     const snapshot = await db.collection("Event").get();
 
@@ -65,7 +68,7 @@ async function sendEmails() {
 }
 
 function sendEmail(toEmail, message, subject) {
-  console.log("ID ",EMAILJS_USER_ID);
+  console.log("ID ", EMAILJS_USER_ID);
 
   try {
     emailjs.init(EMAILJS_USER_ID);
